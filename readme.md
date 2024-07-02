7e42441901bc5d1b6c72ae5a0f5e87c96e250fc8
fsrcodefsr <fsrcodefsr@gmail.com>
2024-07-02 09:53:23 +0500

    commit v1

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Spring App Config Server
+tutorial: https://gitlab.com/spring-apps-infra/spring-app-config-server
+
+local impl. pulling configs from gitlab
---
+++ spring-app-config-server/src/test/java/org/demo/spring_app_config_server/SpringAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.spring_app_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
